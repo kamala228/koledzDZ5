08de6abbbf6f0cfd48deb8ea7d49336ed9a38c77
Lenovo <Lenovo@DESKTOP-1RER6FA>
2025-09-20 11:27:47 +0300

    Добавьте файлы проекта.

---
+++ koledzDZ5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace koledzDZ5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cislo1;
+            Console.Write("ведите число: ");
+            cislo1 = Convert.ToInt32(Console.ReadLine());
+            if (cislo1 % 2 == 0)
+            {
+                Console.WriteLine("число парное ");
+            }
+            else
+            {
+                Console.WriteLine("число не парное ");
+            }
+            if (cislo1<0){
+                Console.WriteLine("число отрицательное ");
+            }
+            else
+            {
+                Console.WriteLine("число положительное ");
+            }
+        }
+    }
+}
